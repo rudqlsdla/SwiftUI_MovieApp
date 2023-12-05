5b9ce63dec98952417270da9bf5d909bdcf1e6f5
Gyeongbin Im <hsmikvyv@gmail.com>
2023-12-05 20:43:50 +0900

    Initial Commit

---
+++ iOSMovie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSMovie
+//
+//  Created by 임경빈 on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSMovie/iOSMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMovieApp.swift
+//  iOSMovie
+//
+//  Created by 임경빈 on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
