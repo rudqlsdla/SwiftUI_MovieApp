f85b3ad5aeafca12c9afa461d0374970056f545a
happyyyyyyyyyy <hsmikvyv@gmail.com>
2024-01-11 00:26:27 +0900

    feat($home): api 통신 중 loading indicator를 위한 LottieView 추가

--- iOSMovie/Feature/Home/HomeView.swift
+++ iOSMovie/Feature/Home/HomeView.swift
@@ -5,25 +5,49 @@
 //  Created by 임경빈 on 1/3/24.
 //
 
+import Lottie
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-      ZStack {
-        Color.blue.opacity(0.3)
-          .ignoresSafeArea(.all)
-        Text("Home")
-          .task {
-            do {
-              print(try await MovieClient().getTopRatedMovie())
-            } catch {
-              print(error)
+  @State private var topRatedMovieList: [Datum] = []
+  @State private var isLoading = false
+  
+  var body: some View {
+    ZStack {
+      Color.blue.opacity(0.3)
+        .ignoresSafeArea(.all)
+      
+      if isLoading {
+        LottieView(animation: .named("Loading_Lottie"))
+          .looping()
+          .scaleEffect(4)
+      }
+      
+      ScrollView(.horizontal) {
+        HStack {
+          ForEach(topRatedMovieList) { movie in
+            VStack {
+              Text("\(movie.title)")
+              Text("\(movie.popularity)")
+              Text("\(movie.releaseDate)")
             }
           }
+        }
+      }
+      .background(.green)
+      .task {
+        do {
+          isLoading.toggle()
+          topRatedMovieList = try await MovieClient().getTopRatedMovie()
+          isLoading.toggle()
+        } catch {
+          print(error)
+        }
       }
     }
+  }
 }
 
 #Preview {
-    HomeView()
+  HomeView()
 }
