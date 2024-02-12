a716461dca22a4103f68a6491a03f781633dd2fc
happyyyyyyyyyy <hsmikvyv@gmail.com>
2024-02-13 00:55:20 +0900

    feat($detail): 영화 상영시간 정보 추가

--- iOSMovie/Feature/Common/MovieDetailView.swift
+++ iOSMovie/Feature/Common/MovieDetailView.swift
@@ -11,8 +11,11 @@ import SwiftUI
 
 struct MovieDetailView: View {
   let movie: Movie
+  @State private var movieInfo: MovieInfo?
   @State private var genreList: [String] = []
+  @State private var genreText: String = ""
   @State private var detailText: String = ""
+  
   
   var body: some View {
     VStack(spacing: 10) {
@@ -29,11 +32,14 @@ struct MovieDetailView: View {
 //                         startPoint: .top,
 //                         endPoint: .bottom)
 //        )
-      
+      		
       VStack(spacing: 8) {
         Text(movie.title)
           .multilineTextAlignment(.center)
           .font(.system(size: 24, weight: .bold))
+        
+        Text(genreText)
+          .font(.system(size: 14, weight: .light))
         
         Text(detailText)
           .font(.system(size: 14, weight: .light))
@@ -43,17 +49,38 @@ struct MovieDetailView: View {
           .lineLimit(3)
           .expandButton(TextSet(text: "more", font: .body, color: .blue))
           .collapseButton(TextSet(text: "less", font: .body, color: .blue))
-          .expandAnimation(.easeOut(duration: 0.5))
       }
       Spacer()
     }
     .padding(20)
     .onAppear {
-      genreList = GenreInfo.shared.idToName(idArray: movie.genreIDS)
-      for genre in genreList {
-        detailText += genre + " · "
-      }
-      detailText += movie.releaseDate
+      getGenre()
+      getDetailInfo()
+    }
+  }
+  
+  func getGenre() {
+    genreList = GenreInfo.shared.idToName(idArray: movie.genreIDS)
+    for genre in genreList {
+      genreText += genre + " · "
+    }
+    genreText = String(genreText.dropLast(3))
+  }
+  
+  func getDetailInfo() {
+    Task {
+      print(movie.id)
+      do {
+        if let info = try await MovieClient().getDetailInfo(movieID: movie.id) {
+          movieInfo = info
+          print(movieInfo)
+          if let movieInfo {
+            detailText = "\(movieInfo.runtime)min · \(movieInfo.releaseDate)"
+          }
+        }
+      } catch {
+        print(error)
+      } 
     }
   }
 }
