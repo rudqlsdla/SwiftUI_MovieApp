32d9cb05cd5e00e268c3a07404827d92d86b6ce9
happyyyyyyyyyy <hsmikvyv@gmail.com>
2024-02-18 15:31:58 +0900

    chore: genre 받아오는 로직 더 명확하게 변경

--- iOSMovie/Shared/GenreInfo.swift
+++ iOSMovie/Shared/GenreInfo.swift
@@ -15,11 +15,11 @@ class GenreInfo {
   private init() { }
   
   func idToName(idArray: [Int]) -> [String] {
-    let genreArray: [(Int, String)] = genreList?.genres.map { ($0.id, $0.name) } ?? []
+    let genreArray: [(id: Int, name: String)] = genreList?.genres.map { ($0.id, $0.name) } ?? []
     let resultArray = idArray.flatMap { id in // flatMap으로 2차원 배열을 단일 배열로 치환
       genreArray.filter {
-        $0.0 == id // 선택한 영화의 장르 id와 기준 장르 id 비교
-      }.map { $0.1 } // 튜플 두 번째 받아오기 -> name
+        $0.id == id // 선택한 영화의 장르 id와 기준 장르 id 비교
+      }.map { $0.name } // 튜플 두 번째 받아오기 -> name
     }
     return resultArray
   }
